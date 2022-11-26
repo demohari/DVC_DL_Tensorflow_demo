7be7da8af084ec8c2cdad30e31d4cc54b557ca6c
demohari <kishore_koneru@yahoo.com>
2022-11-26 19:55:12 +0000

    statge01 done

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -8,7 +8,8 @@ from src.utils.all_utils import read_yaml, create_directory
 
 logging_str = "[%(asctime)s: %(levelname)s:%(module)s]:%(message)s"
 log_dir = "logs"
-create_directory([log_dir])
+#create_directory([log_dir])
+os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str,filemode='a')
 
 
@@ -30,9 +31,9 @@ def get_data(config_path):
     config = read_yaml(config_path)
 
     source_download_dirs = config["source_download_dirs"]
-    print("source: ", source_download_dirs)
+    #print("source: ", source_download_dirs)
     local_data_dirs = config["local_data_dirs"]
-    print("local: ", local_data_dirs)
+    #print("local: ", local_data_dirs)
 
     for source_download_dir, local_data_dir in tqdm(
         zip(source_download_dirs, local_data_dirs),
@@ -41,7 +42,7 @@ def get_data(config_path):
         colour="red",
     ):
         create_directory([local_data_dir])
-        print("local: ", local_data_dirs)
+        #print("local: ", local_data_dirs)
         copy_file(source_download_dir, local_data_dir)
 
 
@@ -53,9 +54,9 @@ if __name__ == "__main__":
     parsed_args = args.parse_args()
 
     try:
-        logging.info("stage01 started")
+        logging.info(">>>>>>>>>>>stage01 started")
         get_data(config_path=parsed_args.config)
-        logging.info("stage01 completed successfully data saved in local")
+        logging.info("stage01 completed successfully data saved in local <<<<<<<<<<<<<")
     except Exception as e:
         logging.exception(e)
         raise e
